e1df15aafce1282a3a24d7a60d329636a5a6c5ad
Steven Wisniewski <steven.w82@googlemail.com>
2015-04-12 13:11:25 +0100

    Random Name Generator
    Type error fixed!

--- random_names.py
+++ random_names.py
@@ -5,10 +5,22 @@ import random
 print 'Random Name Generator\n\n'
 
 # Arrays that hold the different name sections.
-firstNames = ['Steven', 'Alan', 'Craig', 'Billy', 'Scott', 'Adam', 'Robert', 'Indiana', 'Eddie', 'Chris']
-middleNames = ['Bob', 'Shug', 'Dee', 'Gerry', 'Bill', 'Donald', 'Han', 'Luke', 'Ben', 'Al']
-lastNames = ['Wisniewski', 'Honey', 'Farris', 'Gorman', 'Williams', 'Dennis', 'Hendricks', 'Lenny', 'McKay', 'Rotham']
+firstNames = ['Steven', 'Alan', 'Craig', 'Billy', 'Scott',
+			  'Adam', 'Robert', 'Indiana', 'Eddie', 'Chris',
+			  'Ainsley', 'Wade','Walden', 'Nadine', 'Napoleon']
+
+middleNames = ['Bob', 'Shug', 'Dee', 'Gerry', 'Bill', 
+			   'Donald', 'Han', 'Luke', 'Ben', 'Alfonzo',
+			   'Naozumi', 'Gabe', 'Jack', 'Eadric', 'Wagner']
+
+lastNames = ['Wisniewski', 'Honey', 'Farris', 'Gorman', 'Williams',
+			 'Dennis', 'Hendricks', 'Lenny', 'McKay', 'Rotham',
+			 'Daniels', 'Glen', 'Kane', 'Patterson', 'Mahaney']
 
 # The following code selects a listed item at random then prints it to the console
-a, b, c = random.choice(firstNames, middleNames, lastNames)
+
+a = random.choice(firstNames)
+b = random.choice(middleNames)
+c = random.choice(lastNames)
+
 print '%s %s %s' % (a, b, c)
