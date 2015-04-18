42deed8f63b1564000c242f0f7182f4d40a347c7
Steven Wisniewski <steven.w82@googlemail.com>
2015-04-18 04:34:56 +0100

    Takes a string as a command-line argument and counts how many letters there are.

---
+++ Word_count.py
@@ -0,0 +1,13 @@
+# This script takes a command-line arguement and counts how many letters 
+# are in the given string.
+
+import sys
+
+def main(userName, count):
+	for i in userName:
+		count += 1
+		print "%s is letter No.%d" % (i, count)
+
+if __name__ == '__main__':
+	count  = 0
+	main(sys.argv[1], count)
